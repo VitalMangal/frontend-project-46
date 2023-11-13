9b132fa4a6f26eb4f8be481e8f9657861623a24d
VitalMangal <rvi90@bk.ru>
2023-11-13 19:19:19 +0500

    autosave

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,22 @@
+import gendiff from '../src/utils/gendiff.js';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import * as fs from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('flat file test', () => {
+    const expFilePath1 = getFixturePath('expect_file1.json');
+    const expFilePath2 = getFixturePath('expect_file2.json');
+    const resFilePath = getFixturePath('receive_file1.txt');
+
+    const getGenDiff = gendiff(expFilePath1, expFilePath2);
+    const result = fs.readFileSync(resFilePath);
+
+    expect(getGenDiff).toEqual(result);
+    expect(gendiff('')).toThrow();
+    console.log('Тесты прошли успешно');
+});
