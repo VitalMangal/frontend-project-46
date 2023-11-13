88e04cd945fd880fc2bafcaa18abe6b05f7864c3
VitalMangal <rvi90@bk.ru>
2023-11-13 19:46:36 +0500

    Step 5 completed

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,7 @@
-import gendiff from '../src/utils/gendiff.js';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
 import * as fs from 'node:fs';
+import gendiff from '../src/utils/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,14 +9,13 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('flat file test', () => {
-    const expFilePath1 = getFixturePath('expect_file1.json');
-    const expFilePath2 = getFixturePath('expect_file2.json');
-    const resFilePath = getFixturePath('receive_file1.txt');
+  const expFilePath1 = getFixturePath('expect_file1.json');
+  const expFilePath2 = getFixturePath('expect_file2.json');
+  const resFilePath = getFixturePath('receive_file1.txt');
 
-    const getGenDiff = gendiff(expFilePath1, expFilePath2);
-    const result = fs.readFileSync(resFilePath);
+  const getGenDiff = gendiff(expFilePath1, expFilePath2);
+  const result = fs.readFileSync(resFilePath, 'utf-8');
 
-    expect(getGenDiff).toEqual(result);
-    expect(gendiff('')).toThrow();
-    console.log('Тесты прошли успешно');
+  expect(getGenDiff).toEqual(result);
+  console.log('Тесты прошли успешно');
 });
--- src/utils/parse.js
+++ src/utils/parse.js
@@ -3,7 +3,7 @@ import normalizePath from './normalazePath.js';
 
 const parseFile = (filePath) => {
   const nomalizedPath = normalizePath(filePath);
-  const contentFile = fs.readFileSync(nomalizedPath, 'utf8');
+  const contentFile = fs.readFileSync(nomalizedPath);
   const parseContentFile = JSON.parse(contentFile);
   return parseContentFile;
 };
