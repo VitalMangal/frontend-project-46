3c40f5641fa42ec2d72d2e53ad36c99652b32206
VitalMangal <rvi90@bk.ru>
2023-11-17 16:58:00 +0500

    For checking

--- src/utils/gendiff.js
+++ src/utils/gendiff.js
@@ -6,31 +6,24 @@ const gendiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
 
-  const getValue = (value) => {
-    if (_.isObject(value)) {
-      return newTree(value, value);
-    }
-    return value;
-  };
-
   const newTree = (obj1, obj2) => {
     const keys1 = Object.keys(obj1);
     const keys2 = Object.keys(obj2);
     const keys = _.sortBy(_.union(keys1, keys2));
     const childrens = keys.map((key) => {
       if (!Object.hasOwn(obj1, key)) {
-        return { key, value: getValue(obj2[key]), status: 'add' };
+        return { key, value: (_.isObject(obj2[key]) ? newTree(obj2[key], obj2[key]) : obj2[key]), status: 'add' };
       }
       if (!Object.hasOwn(obj2, key)) {
-        return { key, value: getValue(obj1[key]), status: 'remove' };
+        return { key, value: (_.isObject(obj1[key]) ? newTree(obj1[key], obj1[key]) : obj1[key]), status: 'remove' };
       }
       if (_.isEqual(obj1[key], obj2[key])) {
-        return { key, value: getValue(obj1[key]), status: 'unchange' };
+        return { key, value: (_.isObject(obj1[key]) ? newTree(obj1[key], obj1[key]) : obj1[key]), status: 'unchange' };
       }
       if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
         return { key, value: newTree(obj1[key], obj2[key]), status: 'changeObjs' };
       }
-      return { key, value: { obj1Key: getValue(obj1[key]), obj2Key: getValue(obj2[key]) }, status: 'change' };
+      return { key, value: { obj1Key: (_.isObject(obj1[key]) ? newTree(obj1[key], obj1[key]) : obj1[key]), obj2Key: (_.isObject(obj2[key]) ? newTree(obj2[key], obj2[key]) : obj2[key]) }, status: 'change' };
     });
     return childrens;
   };
