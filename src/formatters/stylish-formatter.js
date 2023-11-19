4d52699ab0a4ab69977e17f253aad3dd17d81367
VitalMangal <rvi90@bk.ru>
2023-11-19 14:07:47 +0500

    autosave

--- src/formatters/stylish-formatter.js
+++ src/formatters/stylish-formatter.js
@@ -4,7 +4,22 @@ const specСhars = {
   unchange: '  ',
   add: '+ ',
   remove: '- ',
-  changeObjs: '  ',
+  changeObj: '  ',
+};
+
+const displayValue = (value, depth) => {
+  const currentIndent = '  '.repeat(depth * 2);
+  const bracketIndent = '  '.repeat(depth * 2 - 2);
+  if (_.isObject(value)) {
+    const keys = Object.keys(value);
+    const lines = keys.map((key) => `${currentIndent}${key}: ${displayValue(value[key], depth + 1)}`);
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  }
+  return value;
 };
 
 const formatterStylish = (diffTree) => {
@@ -12,16 +27,13 @@ const formatterStylish = (diffTree) => {
     const currentIndent = '  '.repeat(depth * 2 - 1);
     const bracketIndent = '  '.repeat(depth * 2 - 2);
     const lines = tree.map((obj) => {
-      if (_.isObject(obj.value)) {
-        if (obj.status === 'change') {
-          return `${currentIndent}${specСhars.remove}${obj.key}: ${(_.isObject(obj.value.obj1Key) ? iter(obj.value.obj1Key, depth + 1) : obj.value.obj1Key)}\n${currentIndent}${specСhars.add}${obj.key}: ${(_.isObject(obj.value.obj2Key) ? iter(obj.value.obj2Key, depth + 1) : obj.value.obj2Key)}`;
-        }
+      if (obj.status === 'changeObj') {
         return `${currentIndent}${specСhars[obj.status]}${obj.key}: ${iter(obj.value, depth + 1)}`;
       }
       if (obj.status === 'change') {
-        return `${currentIndent}${specСhars.remove}${obj.key}: ${obj.value.obj1Key}\n${currentIndent}${specСhars.add}${obj.key}: ${obj.value.obj2Key}`;
+        return `${currentIndent}${specСhars.remove}${obj.key}: ${displayValue(obj.value.obj1Key, depth + 1)}\n${currentIndent}${specСhars.add}${obj.key}: ${displayValue(obj.value.obj2Key, depth)}`;
       }
-      return `${currentIndent}${specСhars[obj.status]}${obj.key}: ${obj.value}`;
+      return `${currentIndent}${specСhars[obj.status]}${obj.key}: ${displayValue(obj.value, depth + 1)}`;
     });
     return [
       '{',
--- src/utils/diffTree.js
+++ src/utils/diffTree.js
@@ -11,21 +11,22 @@ const diffTree = (filepath1, filepath2) => {
     const keys = _.sortBy(_.union(keys1, keys2));
     const childrens = keys.map((key) => {
       if (!Object.hasOwn(obj1, key)) {
-        return { key, value: (_.isObject(obj2[key]) ? newTree(obj2[key], obj2[key]) : obj2[key]), status: 'add' };
+        return { key, value: obj2[key], status: 'add' };
       }
       if (!Object.hasOwn(obj2, key)) {
-        return { key, value: (_.isObject(obj1[key]) ? newTree(obj1[key], obj1[key]) : obj1[key]), status: 'remove' };
-      }
-      if (_.isEqual(obj1[key], obj2[key])) {
-        return { key, value: (_.isObject(obj1[key]) ? newTree(obj1[key], obj1[key]) : obj1[key]), status: 'unchange' };
+        return { key, value: obj1[key], status: 'remove' };
       }
       if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-        return { key, value: newTree(obj1[key], obj2[key]), status: 'changeObjs' };
+        return { key, value: newTree(obj1[key], obj2[key]), status: 'changeObj' };
       }
-      return { key, value: { obj1Key: (_.isObject(obj1[key]) ? newTree(obj1[key], obj1[key]) : obj1[key]), obj2Key: (_.isObject(obj2[key]) ? newTree(obj2[key], obj2[key]) : obj2[key]) }, status: 'change' };
+      if (obj1[key] === obj2[key]) {
+        return { key, value: obj1[key], status: 'unchange' };
+      }
+      return { key, value: { obj1Key: obj1[key], obj2Key: obj2[key] }, status: 'change' };
     });
     return childrens;
   };
+
   return newTree(data1, data2);
 };
 
